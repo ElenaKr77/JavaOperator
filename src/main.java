25a681e5b215ef884ace408599f0bf76f24fc123
ElenaKr77 <elena.kriventsova77@gmail.com>
2025-04-23 23:42:30 +0300

    src1

--- src/main.java
+++ src/main.java
@@ -1,2 +1,13 @@
 public class main {
+    public static void main(String[] args) {
+    int i = 100; // начальная сумма на счете клиента
+    int x = 1100; // сумма пополнения
+    int b = x / 100; // бонусы
+    if ( x >= 1000 ) {
+        System.out.println("Количество бонусных рублей " + b );
+        System.out.println("Итоговый счет " + (i + x + b));
+    } else {
+        System.out.println("Итоговый счет " + (i + x));
+    }
+}
 }
